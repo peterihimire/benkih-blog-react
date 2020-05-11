ca7e30dadc89eb7535e3e1925ee1d83c5eea3771
peterihimire <peterihimire@gmail.com>
2020-05-11 23:35:07 +0100

    added env.development file to secure my access token which i added to gitignore

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,10 @@
 import { createClient } from "contentful";
 
 export default createClient({
-  space: "vbo904es5jbj",
-  accessToken: "W0C2NtPDhwwm7SKyvGbd4OKQKtirmHxxYzqv9PTjyE0"
+  // space: "vbo904es5jbj",
+  // accessToken: "W0C2NtPDhwwm7SKyvGbd4OKQKtirmHxxYzqv9PTjyE0"
+  space: process.env.REACT_APP_API_SPACE,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN
 });
+// space: process.env.REACT_APP_API_SPACE,
+// accessToken: process.env.REACT_APP_ACCESS_TOKEN
